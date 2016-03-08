328dd9010c3568f4b99b4fd2e00cba33c0e41f78
Cattani Simone <info@cattanisimone.it>
2016-03-08 09:26:51 +0100

    new function

--- hash.cpp
+++ hash.cpp
@@ -13,27 +13,10 @@ using namespace boost::assign;
 
 void dimension_hash(const Value** args, Value* res, void*)
 {
-	int64_t i,n;
-	double r;
-	stringstream ss;
-
-    n = args[0]->getInt64();
-	r = 0;
-	ss << n;
-
-	if(n>1)
-		for(i=2;i<=sqrt(n);i++)
-			if(n%i==0)
-			{
-				r = 1;
-				break;
-			}
-
-	if(r==0 && n>1)
+	if(1)
 		res->setInt64(1);
 	else
  		res->setInt64(0);
-
 }
 
-REGISTER_FUNCTION(dim_hash, list_of("int64"), "int64", dimension_hash);
+REGISTER_FUNCTION(dim_hash, list_of("int64"), "string", dimension_hash);
