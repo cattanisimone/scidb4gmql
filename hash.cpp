b30742a651a82466990100b49475c2d7b8d8ae76
Cattani Simone <info@cattanisimone.it>
2016-03-08 12:15:14 +0100

    new function

--- hash.cpp
+++ hash.cpp
@@ -112,12 +112,12 @@ int keccak(const uint8_t *in, int inlen, uint8_t *md, int mdlen)
         keccakf(st, KECCAK_ROUNDS);
     }
   */  
-    // last block and padding
+/*    // last block and padding
     memcpy(temp, in, inlen);
     temp[inlen++] = 1;
     memset(temp + inlen, 0, rsiz - inlen);
     temp[rsiz - 1] |= 0x80;
-/*
+
     for (i = 0; i < rsizw; i++)
         st[i] ^= ((uint64_t *) temp)[i];
 
